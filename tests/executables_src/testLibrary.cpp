54545d1e1beb49b2ce71df7e5479d0dd3d3c5ff6
mhier <mhier@0c6bac80-c947-4e08-a159-8bd5db5f324c>
2015-08-20 13:42:00 +0000

    added more test cases for the library: test exceptions when opening the device and test writing some configuration registers (more needed...)
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u_applications/ad16/trunk@39 0c6bac80-c947-4e08-a159-8bd5db5f324c

--- tests/executables_src/testLibrary.cpp
+++ tests/executables_src/testLibrary.cpp
@@ -29,6 +29,7 @@ class Ad16Test {
     }
 
     void testExceptions();
+    void testTriggerModes();
     void testSoftwareTriggeredMode();
     void testPeriodicTriggeredMode();
 
@@ -44,6 +45,7 @@ class Ad16TestSuite : public test_suite {
     Ad16TestSuite() : test_suite("DummyDevice test suite"){
       boost::shared_ptr<Ad16Test> ad16Test( new Ad16Test );
       add( BOOST_CLASS_TEST_CASE( &Ad16Test::testExceptions, ad16Test ) );
+      add( BOOST_CLASS_TEST_CASE( &Ad16Test::testTriggerModes, ad16Test ) );
       add( BOOST_CLASS_TEST_CASE( &Ad16Test::testSoftwareTriggeredMode, ad16Test ) );
       add( BOOST_CLASS_TEST_CASE( &Ad16Test::testPeriodicTriggeredMode, ad16Test ) );
     }
@@ -80,8 +82,37 @@ void Ad16Test::testExceptions() {
     BOOST_CHECK( a.getID() == ad16Exception::NOT_OPENED);
   }
 
+  // open /dev/null as device
+  BOOST_CHECK_THROW( ad.open("/dev/null",TEST_MAPPING_FILE) , ad16Exception);
+  try {
+    ad.open("/dev/null",TEST_MAPPING_FILE);
+  }
+  catch(ad16Exception &a) {
+    BOOST_CHECK( a.getID() == ad16Exception::CANNOT_OPEN);
+  }
+
+  // open non-existing dmap file
+  BOOST_CHECK_THROW( ad.openDmap("this_file_does_not_exist","AD16") , ad16Exception);
+  try {
+    ad.openDmap("this_file_does_not_exist","AD16");
+  }
+  catch(ad16Exception &a) {
+    BOOST_CHECK( a.getID() == ad16Exception::CANNOT_OPEN);
+  }
+
+  // open dummy-PCIe device (wrong type of PCIe device)
+  BOOST_CHECK_THROW( ad.open("/dev/mtcadummys0",TEST_MAPPING_FILE) , ad16Exception);
+  ad.close();
+  try {
+    ad.open("/dev/mtcadummys0",TEST_MAPPING_FILE);
+  }
+  catch(ad16Exception &a) {
+    BOOST_CHECK( a.getID() == ad16Exception::CANNOT_OPEN);
+  }
+  ad.close();
+
   // open device
-  ad.open(TEST_MAPPING_FILE,TEST_MAPPING_FILE);
+  ad.openDmap("ad16.dmap","DUMMY");
 
   // open a second time: should throw exception
   BOOST_CHECK_THROW( ad.open(TEST_MAPPING_FILE,TEST_MAPPING_FILE) , ad16Exception);
@@ -157,6 +188,60 @@ void Ad16Test::testExceptions() {
   catch(ad16Exception &a) {
     BOOST_CHECK(a.getID() == ad16Exception::CHANNEL_OUT_OF_RANGE);
   }
+
+  // close device
+  ad.close();
+
+}
+
+/**********************************************************************************************************************/
+void Ad16Test::testTriggerModes() {
+  std::cout << "testTriggerModes" << std::endl;
+  mapFile::mapElem elem;
+  int32_t val;
+
+  // open device
+  ad.open(TEST_MAPPING_FILE,TEST_MAPPING_FILE);
+
+  // test periodic trigger mode
+  ad.setTriggerMode(ad16::PERIODIC,1.);
+  ad._map->getRegisterInfo("WORD_TIMING_TRG_SEL", elem, "APP0");
+  ad._dummyDevice->readReg(elem.reg_address,&val,elem.reg_bar);
+  BOOST_CHECK( val == 0 );
+  ad._map->getRegisterInfo("WORD_TIMING_INT_ENA", elem, "APP0");
+  ad._dummyDevice->readReg(elem.reg_address,&val,elem.reg_bar);
+  BOOST_CHECK( (val & (1<<0)) != 0 );
+  ad._map->getRegisterInfo("WORD_TIMING_FREQ", elem, "APP0");
+  ad._dummyDevice->readReg(elem.reg_address,&val,elem.reg_bar);
+  BOOST_CHECK( val == 49999999 );               // 1 Hz
+
+  // test user trigger mode
+  ad.setTriggerMode(ad16::USER);
+  ad._map->getRegisterInfo("WORD_TIMING_TRG_SEL", elem, "APP0");
+  ad._dummyDevice->readReg(elem.reg_address,&val,elem.reg_bar);
+  BOOST_CHECK( val == 8 );
+  ad._map->getRegisterInfo("WORD_TIMING_INT_ENA", elem, "APP0");
+  ad._dummyDevice->readReg(elem.reg_address,&val,elem.reg_bar);
+  BOOST_CHECK( (val & (1<<8)) == 0 );
+
+  // test external trigger mode on channel 3
+  ad.setTriggerMode(ad16::EXTERNAL,3);
+  ad._map->getRegisterInfo("WORD_TIMING_TRG_SEL", elem, "APP0");
+  ad._dummyDevice->readReg(elem.reg_address,&val,elem.reg_bar);
+  BOOST_CHECK( val == 3 );
+  ad._map->getRegisterInfo("WORD_TIMING_INT_ENA", elem, "APP0");
+  ad._dummyDevice->readReg(elem.reg_address,&val,elem.reg_bar);
+  BOOST_CHECK( (val & (1<<3)) == 0 );
+
+  // test external trigger mode on channel 0 after the PERIODIC trigger was enabled (they are on the same channel)
+  ad.setTriggerMode(ad16::PERIODIC,1.);
+  ad.setTriggerMode(ad16::EXTERNAL,0);
+  ad._map->getRegisterInfo("WORD_TIMING_TRG_SEL", elem, "APP0");
+  ad._dummyDevice->readReg(elem.reg_address,&val,elem.reg_bar);
+  BOOST_CHECK( val == 0 );
+  ad._map->getRegisterInfo("WORD_TIMING_INT_ENA", elem, "APP0");
+  ad._dummyDevice->readReg(elem.reg_address,&val,elem.reg_bar);
+  BOOST_CHECK( (val & (1<<0)) == 0 );
 
   // close device
   ad.close();
